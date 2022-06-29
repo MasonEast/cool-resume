c98d66cf1195926c08ea0a97e94158999a58d905
mason <2224742726@qq.comm>
2022-06-29 13:43:32 +0800

    使用tailwind css

---
+++ src/page/home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-green-700">1</header>
+      <div className="flex justify-between">
+        <div>44</div>
+        <div>33</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
